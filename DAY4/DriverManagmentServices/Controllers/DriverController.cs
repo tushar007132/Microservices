174ef45fea9eb04b7ea065782cf5eb00c99bc413
tushar007132 <tusharkumar0308@gmail.com>
2022-08-04 15:38:05 +0530

    Add files via upload
    Uber Microservices Implementation

---
+++ DAY4/DriverManagmentServices/Controllers/DriverController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DriverManagmentServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+
+        List<DriverModel> drlist = new List<DriverModel>();
+
+        // GET: api/<DriverController>
+        [HttpGet]
+
+        public IEnumerable<DriverModel> Get()
+        {
+            DriverModel dr1 = new DriverModel();
+            DriverModel dr2 = new DriverModel();
+
+            dr1.DriverId = 101;
+            dr1.DriverName = "Raju";
+
+            dr2.DriverId = 102;
+            dr2.DriverName = "Rajesh";
+
+            //List<DriverModel> drlist = new List<DriverModel>();
+            
+            drlist.Add(dr1);
+            drlist.Add(dr2);
+            return drlist;
+        }
+
+
+
+        // GET api/<DriverController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            
+            return "value";
+        }
+
+        // POST api/<DriverController>
+        [HttpPost]
+        public void Post([FromBody] DriverModel drobj)
+        {
+            //List<DriverModel> drlist = new List<DriverModel>();
+
+            drlist.Add(drobj);
+        }
+
+        // PUT api/<DriverController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] DriverModel drobj)
+        {
+            var data = drlist.FirstOrDefault(x => x.DriverId == id);
+            if (data != null)
+            {
+                data.DriverId = drobj.DriverId;
+                data.DriverName = drobj.DriverName;
+            }
+        }
+
+        // DELETE api/<DriverController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var data = drlist.FirstOrDefault(x => x.DriverId == id);
+            drlist.Remove(data);
+        }
+    }
+}
---
+++ DAY4/PassangerManagmentServices/Controllers/PassangerController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PassangerManagmentServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PassangerController : ControllerBase
+    {
+
+        List<PassangerModel> psglist = new List<PassangerModel>();
+        // GET: api/<PassangerController>
+        [HttpGet]
+       
+
+        public IEnumerable<PassangerModel> Get()
+        {
+            PassangerModel psg1 = new PassangerModel();
+            PassangerModel psg2 = new PassangerModel();
+
+            psg1.PassangerId = 101;
+            psg1.PassangerName = "Tushar";
+
+
+            psg1.PassangerId = 102;
+            psg1.PassangerName = "Akash";
+
+            psglist.Add(psg1);
+            psglist.Add(psg2);
+
+            return psglist;
+        }
+
+
+        // GET api/<PassangerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PassangerController>
+        [HttpPost]
+        public void Post([FromBody] PassangerModel drobj)
+        {
+            //List<DriverModel> drlist = new List<DriverModel>();
+
+            psglist.Add(drobj);
+        }
+        // PUT api/<PassangerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] PassangerModel drobj)
+        {
+            var data = psglist.FirstOrDefault(x => x.PassangerId == id);
+            if (data != null)
+            {
+                data.PassangerId = drobj.PassangerId;
+                data.PassangerName = drobj.PassangerName;
+            }
+        }
+
+        // DELETE api/<PassangerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var data = psglist.FirstOrDefault(x => x.PassangerId == id);
+            psglist.Remove(data);
+        }
+    }
+}
